8783e4cb8728e1b17e8d50476ec5450be01b0f36
TylerM17 <84191365+223tmorris@users.noreply.github.com>
2022-10-03 13:12:40 +0000

    switch statements

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // switch statements
-const grade = 'D';
+// const grade = '50';
 
-//using if statements
-if(grade === 'A'){
-  
-}else if(grade === 'B'){
-  
-}else if(grade === 'C'){
-  
-}else if(grade === 'D'){
-  
-}else if(grade === 'E'){
-  
-}else{
+// switch (grade) {
+//   case '50':
+//     console.log('you got an A!');
+//     break;
+//   case 'B':
+//     console.log('you got a B!');
+//     break;
+//   case 'C':
+//     console.log('you got a C!');
+//     break;
+//   case 'D':
+//     console.log('you got a D!');
+//     break;
+//   case 'E':
+//     console.log('you got an E!');
+//     break;
+//   default:
+//     console.log('not a valid grade');
+// }
+
+
+
+
+
+// variables & block scope
+
+if(true){
   
 }
