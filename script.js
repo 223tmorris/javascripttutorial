cd4401c91d32e731a4e282ae814c71d6d8fea2db
TylerM17 <84191365+223tmorris@users.noreply.github.com>
2022-10-03 13:03:09 +0000

    if statements

---
+++ script.js
@@ -0,0 +1,17 @@
+// switch statements
+const grade = 'D';
+
+//using if statements
+if(grade === 'A'){
+  
+}else if(grade === 'B'){
+  
+}else if(grade === 'C'){
+  
+}else if(grade === 'D'){
+  
+}else if(grade === 'E'){
+  
+}else{
+  
+}
