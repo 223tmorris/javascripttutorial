cef65e195ba614203dd2bfdbfbc2923eee4271cf
TylerM17 <84191365+223tmorris@users.noreply.github.com>
2022-10-04 14:19:31 +0000

    returning values

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// switch statements
+// // switch statements
 // const grade = '50';
 
 // switch (grade) {
@@ -25,19 +25,77 @@
 
 
 
-// variables & block scope
-const age = 30;
+// // variables & block scope
+// const age = 30;
 
-if (true) {
-  const age = 40;
-  const name = 'shaun';
-  console.log('inside 1st code block: ', age, name);
+// if (true) {
+//   const age = 40;
+//   const name = 'shaun';
+//   console.log('inside 1st code block: ', age, name);
 
-  if (true) {
-    const age = 50;
-    console.log('inside 2nd code block: ', age);
-    var test = 'hello';
-  }
-}
+//   if (true) {
+//     const age = 50;
+//     console.log('inside 2nd code block: ', age);
+//     var test = 'hello';
+//   }
+// }
 
-console.log('outside code block: ', age, name, test);
+// console.log('outside code block: ', age, name, test);
+
+
+
+
+
+
+
+
+
+
+// tutorial 4 - functions
+
+
+// // function expression
+// const speak = function() {
+//   console.log('good day!');
+// };
+
+// // greet();
+// // greet();
+// // greet();
+
+// speak();
+// speak();
+// speak();
+
+// // function declaration
+// function greet() {
+//   console.log('hello there');
+// }
+
+
+
+
+// // arguments & parameters
+
+// const speak = function(name = 'luigi', time = 'night') {
+//   console.log(`good ${time} ${name}`);
+// };
+
+// speak();
+// speak('shaun');
+
+
+
+
+// returning values
+
+// regular function
+// const calcArea = function(radius) {
+//   return 3.14 * radius **  2;
+// };
+
+// arrow function
+const calcArea = radius => 3.14 * radius ** 2;
+
+const area = calcArea(5);
+console.log('area is: ', area);
