ef0f990aea98ccece8a28ada19e33016d86158b7
TylerM17 <84191365+223tmorris@users.noreply.github.com>
2022-10-06 15:44:59 +0000

    completed tutorial 5

--- script.js
+++ script.js
@@ -201,5 +201,117 @@
 
 
 
-// tutorial 5 - the document object model
+// tutorial 5 - objects
+
+// object literals
+
+// const blogs = [
+//   { title: 'why mac & cheese rules', likes: 30 },
+//   { title: '10 things to make with marmite', likes: 50 }
+// ];
+
+// console.log(blogs);
+
+// let user = {
+//   name: 'crystal',
+//   age: 30,
+//   email: 'crystal@thenetninja.co.uk',
+//   location: 'berlin',
+//   blogs: [
+//     { title: 'why mac & cheese rules', likes: 30 },
+//     { title: '10 things to make with marmite', likes: 50 }
+//   ],
+//   login: function() {
+//     console.log('the user logged in');
+//   },
+//   logout: function() {
+//     console.log('the user logged out');
+//   },
+//   logBlogs: function() {
+//     // console.log(this.blogs);
+//     console.log('this user has written the following blogs:');
+//     this.blogs.forEach(blog => {
+//       console.log(blog.title, blog.likes);
+//     });
+//   }
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// // user.age = 35;
+// console.log(user.age);
+
+// const key = 'location';
+
+// console.log(user[key]);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
+// user.login();
+// user.logout();
+
+// user.logBlogs();
+// console.log(this);
+
+
+
+
+
+// // math object
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// const area = 7.7;
+
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
+
+// // random numbers
+
+// const random = Math.random(0, 100);
+
+// console.log(random);
+// console.log(Math.round(random * 100));
+
+
+
+
+
+// // primitive values
+
+// let scoreOne = 50;
+// let scoreTwo = scoreOne
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// scoreOne = 100;
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// // reference values
+
+// const userOne = { name: 'ryu', age: 30 };
+// const userTwo = userOne;
+
+// console.log(userOne, userTwo);
+
+// userOne.name = 'chun-li';
+// console.log(userOne, userTwo);
+
+
+
+
+
+
+
+
+
+
+// the document object model
 
