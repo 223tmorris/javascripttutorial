e6014bdb83feea8d79d5270a5e6eb22c1426a31a
TylerM17 <84191365+223tmorris@users.noreply.github.com>
2022-10-06 14:56:42 +0000

    callbacks and forEach

--- script.js
+++ script.js
@@ -87,15 +87,88 @@
 
 
 
-// returning values
+// // returning values
 
-// regular function
+// // regular function
 // const calcArea = function(radius) {
 //   return 3.14 * radius **  2;
 // };
 
-// arrow function
-const calcArea = radius => 3.14 * radius ** 2;
+// // arrow function
+// const calcArea = radius => 3.14 * radius ** 2;
 
-const area = calcArea(5);
-console.log('area is: ', area);
+// const area = calcArea(5);
+// console.log('area is: ', area);
+
+// // practice arrow functions
+
+// const greet = function() {
+//   return 'hello, world';
+// };
+
+// const greet = () => 'hello, world';
+// const result = greet();
+// console.log(result);
+
+// const bill = function(products, tax) {
+//   let total = 0;
+//   for (let i = 0; i < products.length; i++) {
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+// console.log(bill([10, 15, 30], 0.2));
+
+// const bill = (products, tax) => {
+//   let total = 0;
+//   for (let i = 0; i < products.length; i++) {
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// };
+
+// console.log(bill([10, 15, 30], 0.2));
+
+
+
+
+
+// const name = 'shaun';
+
+// // functions
+
+// const greet = () => 'hello';
+
+// let resultOne = greet();
+// console.log(resultOne);
+
+// // methods
+
+// let resultTwo = name.toUpperCase();
+// console.log(resultTwo);
+
+
+
+
+
+// // callbacks & forEach
+
+// const myFunc = (callbackFunc) => {
+//   // do something
+//   let value = 50;
+//   callbackFunc(value);
+// };
+
+// myFunc(value => {
+//   // do something
+//   console.log(value);
+// });
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`);
+};
+
+people.forEach(logPerson);
